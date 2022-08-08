5a1777627213b9ff0ad18322c51d55bff225dc3b
Patryk Jasik <zielonyfantom@interia.pl>
2022-08-08 17:21:00 +0200

    Added hurtbox to turrets

--- Scripts/Enemies/Turret.gd
+++ Scripts/Enemies/Turret.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
+signal enemy_killed(position)
+
 export var is_on_floor = true
 var can_fire = true
 var floor_string
@@ -7,19 +9,24 @@ var floor_string
 onready var ray = $RayCast2D
 onready var animation = $AnimationPlayer
 onready var detection_range = $DetectionRange
+onready var hurtbox = $HurtBox
 onready var cooldown = $CooldownTimer
 
 var bullet = preload("res://Scenes/Traps/Bullet.tscn")
 var target = null
 
 func _ready():
+	var _random_var = connect("enemy_killed", get_parent(), "_on_Enemies_enemy_killed")
+	
 	if is_on_floor:
 		floor_string = "floor"
 		detection_range.scale.y = 1
+		hurtbox.scale.y = 1
 		ray.scale.y = -50
 	else:
 		floor_string = "ceil"
 		detection_range.scale.y = -1
+		hurtbox.scale.y = -1
 		ray.scale.y = 50
 		
 
@@ -47,3 +54,8 @@ func _on_DetectionRange_body_exited(body):
 func _on_CooldownTimer_timeout():
 	if target != null:
 		fire()
+
+func _on_HurtBox_body_entered(body):
+	if body.is_in_group("player"):
+		queue_free()
+		emit_signal("enemy_killed", global_position)
