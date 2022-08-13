ea0b86ee1d82f6f9c9c80c767a4ff1efe8555000
Patryk Jasik <zielonyfantom@interia.pl>
2022-08-13 12:50:22 +0200

    Changed how camera get player

--- Scripts/Camera2D.gd
+++ Scripts/Camera2D.gd
@@ -1,6 +1,6 @@
 extends Camera2D
 
-onready var player = get_node("/root/Level1/Player")
+onready var player = get_parent().get_node("Player")
 
 func _process(_delta):
 	position.x = player.position.x
