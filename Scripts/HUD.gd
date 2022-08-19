4fa5898040620a019edf4ba0329e8dc237abb4d7
Patryk Jasik <zielonyfantom@interia.pl>
2022-08-19 22:09:09 +0200

    Segregate the texture folder to subcategories

--- Scripts/HUD.gd
+++ Scripts/HUD.gd
@@ -10,21 +10,21 @@ func _ready():
 	
 	for _i in range(0, max_images):
 		var texture_rect = TextureRect.new()
-		texture_rect.texture = load("res://Assets/Textures/heart_point.png")
+		texture_rect.texture = load("res://Assets/Textures/Collectibles/heart_point.png")
 		life_container.add_child(texture_rect)
 	
 	images = $VBoxContainer/LifeContainer.get_children()
 
 func _on_Player_take_damage():
 	for i in range(images.size() - 1, -1 , -1):
-		if images[i].texture.resource_path != "res://Assets/Textures/blank_heart.png":
-			images[i].texture = load("res://Assets/Textures/blank_heart.png")
+		if images[i].texture.resource_path != "res://Assets/Textures/HUD/blank_heart.png":
+			images[i].texture = load("res://Assets/Textures/HUD/blank_heart.png")
 			break
 
 func _on_Player_heal():
 	for image in images:
-		if image.texture.resource_path != "res://Assets/Textures/heart_point.png":
-			image.texture = load("res://Assets/Textures/heart_point.png")
+		if image.texture.resource_path != "res://Assets/Textures/Collectibles/heart_point.png":
+			image.texture = load("res://Assets/Textures/Collectibles/heart_point.png")
 			break
 
 func _on_Player_coin_collected():
