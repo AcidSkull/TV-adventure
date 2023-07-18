ade8c0d6712bcbc3980f3cadbe689457cc4c30fa
Patryk Jasik <patryk_jasik@o2.pl>
2023-07-18 22:11:18 +0200

    Added main menu button to pause menu

--- Scripts/PauseMenu.gd
+++ Scripts/PauseMenu.gd
@@ -1,10 +1,12 @@
 extends Control
 
 onready var resume = $CenterContainer/VBoxContainer/Resume
+onready var menu = $CenterContainer/VBoxContainer/MainMenu
 onready var exit = $CenterContainer/VBoxContainer/Exit
 
 func _ready():
 	resume.connect("pressed", self, '_on_resume_pressed')
+	menu.connect("pressed", self, '_on_menu_pressed')
 	exit.connect("pressed", self, '_on_exit_pressed')
 
 func _process(_delta):
@@ -19,6 +21,10 @@ func _process(_delta):
 func _on_resume_pressed():
 	hide()
 	get_tree().paused = false
+
+func _on_menu_pressed():
+	get_tree().paused = false
+	var _tmp = get_tree().change_scene("res://Scenes/Mechanics/Menu.tscn")
 
 func _on_exit_pressed():
 	get_tree().quit()
